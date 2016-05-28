a33a084034cd564544a90ffbca6ea38293513fd7
Oleg  Ovcharenko <ovcharoo@kl-16555.kaust.edu.sa>
2016-05-28 14:23:35 +0300

    Mizutani op separate

--- tr_iso_2d_disp_optim_split_op_water.m
+++ tr_iso_2d_disp_optim_split_op_water.m
@@ -11,7 +11,7 @@ clear all;  %clear all variables
  NY =100*mg;  %Y
  
 %  time=0.02d0;
-  time=0.12d0;
+  time=0.16d0;
  
  % time step in seconds
 %  DELTAT = 0.5d-4;	%[sec] 
@@ -678,8 +678,17 @@ for i=2:NX %over OX
 %             coeffuy_dxdy= C(i,j,4)*pre_dxdy(6,:);
             [Aux, Auy] = construct_interface_operators(i,j, gr_x, gr_y, xdscr, ydscr, C, rho);
             
-             coeffux{i,j}=Aux;
-             coeffuy{i,j}=Auy;
+            Aux(1,:) = C(i,j,1)*Aux(1,:);
+            Aux(2,:) = C(i,j,4)*Aux(2,:);
+            Aux(3,:) = C(i,j,2)*Aux(3,:);
+            Aux(4,:) = C(i,j,4)*Aux(4,:);
+            coeffux{i,j}=Aux;
+            
+            Auy(1,:) = C(i,j,4)*Auy(1,:);
+            Auy(2,:) = C(i,j,3)*Auy(2,:);
+            Auy(3,:) = C(i,j,4)*Auy(3,:);
+            Auy(4,:) = C(i,j,2)*Auy(4,:);
+            coeffuy{i,j}=Auy;
             
 %             coeffux{i,j}=[[coeffux_dx2 0]; [coeffux_dy2 0]; coeffux_dxdy];
 %             coeffuy{i,j}=[[coeffuy_dx2 0]; [coeffuy_dy2 0]; coeffuy_dxdy];
@@ -688,72 +697,11 @@ for i=2:NX %over OX
             
         end  %end of if markers(i,j)
         
-        %if any conditions were used use conventional operator
+        %if any conditions were used - use conventional heterogeneous operator
         if isempty(coeffux{i,j}) && isempty(coeffuy{i,j})
-%             %ux_dx2
-%             ml = C(i-1,j,1)+C(i,j,1);                  %left point 
-%             mr = C(i,j,1)+C(i+1,j,1);                  %right point
-%             mc = C(i-1,j,1) +2.d0*C(i,j,1)+C(i+1,j,1); %middle point
-%             coeffux_dx2 = one_over_2dx2*[ml -mc mr];
-%             %uy_dx2
-%             ml = C(i-1,j,4)+C(i,j,4);                  %left point 
-%             mr = C(i,j,4)+C(i+1,j,4);                  %right point
-%             mc = C(i-1,j,4) +2.d0*C(i,j,4)+C(i+1,j,4); %middle point
-%             coeffuy_dx2 = one_over_2dx2*[ml -mc mr];
-%             %ux_dy2
-%             ml = C(i,j-1,4)+C(i,j,4);                  %left point 
-%             mr = C(i,j,4)+C(i,j+1,4);                  %right point
-%             mc = C(i,j-1,4) +2.d0*C(i,j,4)+C(i,j+1,4); %middle point           
-%             coeffux_dy2 = one_over_2dy2*[ml -mc mr];
-%             %uy_dy2
-%             ml = C(i,j-1,3)+C(i,j,3);                  %left point 
-%             mr = C(i,j,3)+C(i,j+1,3);                  %right point
-%             mc = C(i,j-1,3) +2.d0*C(i,j,3)+C(i,j+1,3); %middle point  
-%             coeffuy_dy2 = one_over_2dy2*[ml -mc mr];
-%             
-% %             ux_dxdy
-%             mp1p1 = C(i+1,j+1,2)+C(i,j,2);
-%             mp1m1 = C(i+1,j-1,2)+C(i,j,2);
-%             mm1p1 = C(i-1,j+1,2)+C(i,j,2);
-%             mm1m1 = C(i-1,j-1,2)+C(i,j,2);
-%             coeffux_dxdy= one_over_2dxdy4*[mp1p1 -mp1m1 -mm1p1 mm1m1];
-%             
-% %             uy_dydx
-% %             mp1p1 = C(i+1,j+1,2)+C(i,j,2);
-% %             mp1m1 = C(i+1,j-1,2)+C(i,j,2);
-% %             mm1p1 = C(i-1,j+1,2)+C(i,j,2);
-% %             mm1m1 = C(i-1,j-1,2)+C(i,j,2);
-%             coeffuy_dydx= one_over_2dxdy4*[mp1p1 -mp1m1 -mm1p1 mm1m1];
-%             
-% %             uy_dxdy
-%             mp1p1 = C(i+1,j+1,4)+C(i,j,4);
-%             mp1m1 = C(i+1,j-1,4)+C(i,j,4);
-%             mm1p1 = C(i-1,j+1,4)+C(i,j,4);
-%             mm1m1 = C(i-1,j-1,4)+C(i,j,4);
-%             coeffuy_dxdy= one_over_2dxdy4*[mp1p1 -mp1m1 -mm1p1 mm1m1];
-%             
-% %             ux_dydx
-% %             mp1p1 = C(i+1,j+1,4)+C(i,j,4);
-% %             mp1m1 = C(i+1,j-1,4)+C(i,j,4);
-% %             mm1p1 = C(i-1,j+1,4)+C(i,j,4);
-% %             mm1m1 = C(i-1,j-1,4)+C(i,j,4);
-%             coeffux_dydx= one_over_2dxdy4*[mp1p1 -mp1m1 -mm1p1 mm1m1];
-            
-%             coeffux_dx2 = C(i,j,1)*tmp_dx2;
-%             coeffux_dy2 = C(i,j,4)*tmp_dy2;
-%             coeffux_dxdy= C(i,j,2)*tmp_dxdy;
-      
-%             coeffuy_dx2 = C(i,j,4)*tmp_dx2;
-%             coeffuy_dy2 = C(i,j,3)*tmp_dy2;
-%             coeffuy_dxdy= C(i,j,4)*tmp_dxdy;
-            
-%             coeffux{i,j}=[[coeffux_dx2 0]; [coeffux_dy2 0]; coeffux_dxdy; coeffux_dydx];
-%             coeffuy{i,j}=[[coeffuy_dx2 0]; [coeffuy_dy2 0]; coeffuy_dxdy; coeffuy_dydx];
             [Aux, Auy] = construct_Zahradnik_operators(i,j,C, DELTAX, DELTAY);
             coeffux{i,j} = Aux;
             coeffuy{i,j} = Auy;
-%             coeffuxm(i,j,:,:) = [[coeffux_dx2 0]; [coeffux_dy2 0]; coeffux_dxdy];
-%             coeffuym(i,j,:,:) = [[coeffuy_dx2 0]; [coeffuy_dy2 0]; coeffuy_dxdy];
         end    
     end % end of j loop
 end  %end of i loop
